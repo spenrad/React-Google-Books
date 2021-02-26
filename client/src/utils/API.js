b045eea31bef7c17c6aaa3fa40a5744c432d8b53
William Bryan <wwbryan00@gmail.com>
2021-02-26 13:38:19 -0800

    establish routes and their controllers

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getBooks : function (book) {
-        return axios.get("/google")
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
     }
 }
--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,6 +2,19 @@ const db = require("../models");
 
 module.exports = {
     create: function(req, res) {
-        db.Post.create()
+        db.Post.create(req.body)
+        .then(dbModel = res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    findAll: function(req, res) {
+        db.Post.find(req.query)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.Post.findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
-}
+};
