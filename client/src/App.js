b7362b8bd1f4a56933725d3cef1808845c55158d
Spencer Christy <spenrad@gmail.com>
2021-02-26 11:07:36 -0800

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
+import "./App.css";
 
 
 function App() {
   return (
-    
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved"  component={Saved} />
+        <Route exact path="/" component = {Search} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
