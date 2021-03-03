f6bae31e40503d71e3236c3bbeacc55b2d920f99
Spencer Christy <spenrad@gmail.com>
2021-03-03 15:23:53 -0800

    changed server file paths to match with proper pathing for heroku to deploy successfully

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   // app.use(function (req, res) {
   //   res.sendFile(path.join(__dirname, "../client/build/index.html"))
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
   // })
 }
